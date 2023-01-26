1f41bd179a9c3b3e68be8051e833a0d7eb9ac5a9
Radek Podgorny <radek@podgorny.cz>
2023-01-26 12:16:44 +0100

    blacked setup_win.py

--- setup_win.py
+++ setup_win.py
@@ -3,19 +3,21 @@ from cx_Freeze import setup, Executable
 
 from faddns.version import __version__
 
-base = 'Win32GUI'
+base = "Win32GUI"
 
 setup(
-	name = 'faddns',
-	version = __version__,
-	options = {
-		'build_exe': {
-			'includes': ['re', ],
-			'include_msvcr': True,
-		},
-	},
-	executables = [
-		Executable(script='faddnsc'),
-		#Executable(script='faddnsc_gui'),
-	]
+    name="faddns",
+    version=__version__,
+    options={
+        "build_exe": {
+            "includes": [
+                "re",
+            ],
+            "include_msvcr": True,
+        },
+    },
+    executables=[
+        Executable(script="faddnsc"),
+        # Executable(script='faddnsc_gui'),
+    ],
 )
