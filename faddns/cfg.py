9bebe61bece59b891b3a67de02624827afcc9f15
Radek Podgorny <radek@podgorny.cz>
2023-01-26 12:19:40 +0100

    blacked cfg.py

--- faddns/cfg.py
+++ faddns/cfg.py
@@ -3,28 +3,29 @@ from configparser import ConfigParser
 
 
 class Config:
-	def __init__(self):
-		self.host = socket.gethostname().lower()
-		self.interval = 600
-		self.url = None
+    def __init__(self):
+        self.host = socket.gethostname().lower()
+        self.interval = 600
+        self.url = None
 
-	def read_from_ini(self, fn):
-		ini = ConfigParser()
-		ini.read(fn)
+    def read_from_ini(self, fn):
+        ini = ConfigParser()
+        ini.read(fn)
 
-		self.host = ini.get('General', 'Host', fallback=self.host)
-		self.interval = ini.getint('General', 'Interval', fallback=self.interval)
-		self.url = ini.get('General', 'Url', fallback=self.url)
+        self.host = ini.get("General", "Host", fallback=self.host)
+        self.interval = ini.getint("General", "Interval", fallback=self.interval)
+        self.url = ini.get("General", "Url", fallback=self.url)
 
-	def check(self):
-		if not self.url: return 'no url!'
+    def check(self):
+        if not self.url:
+            return "no url!"
 
-	# TODO: move this to some common module
-	def __str__(self):
-		l = []
-		for k, v in vars(self).items():
-			l.append('%s=\'%s\'' % (k, v))
-		return ', '.join(l)
+    # TODO: move this to some common module
+    def __str__(self):
+        l = []
+        for k, v in vars(self).items():
+            l.append("%s='%s'" % (k, v))
+        return ", ".join(l)
 
 
 cfg = Config()
